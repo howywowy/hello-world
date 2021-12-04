344b7614ba5df813127e5e946c34b46acbdbbfa7
rOOky <howywowy@gmail.com>
2021-12-04 06:19:24 +0100

    Update zomaar.php

--- zomaar.php
+++ zomaar.php
@@ -2,5 +2,6 @@
 echo 'Hallo GitHub';
 echo 'Er is nu een branch voor deze repo';
 echo 'Een eerste merge heeft plaats gevonden, nu de tweede';
-// ik heb nog een echo regel toegevoegd
+echo 'Ik pak de oefeningen weer op 4 dec 2021
+// ik heb op 4 de 2021 nog een echo regel toegevoegd 9regel 5)
 ?>
